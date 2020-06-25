ebf209ee0614203899545ef86e57b10476430071
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-06-25 14:33:30 +0700

    search book v1.0

--- src/app/modules/book/book.component.ts
+++ src/app/modules/book/book.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { BookStorageService } from 'src/app/shared/services/book-storage.service';
-import { Book } from 'src/app/shared/model/book.model';
+import { Component, OnInit } from "@angular/core";
+import { BookStorageService } from "src/app/shared/services/book-storage.service";
+import { Book } from "src/app/shared/model/book.model";
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  selector: "app-book",
+  templateUrl: "./book.component.html",
+  styleUrls: ["./book.component.scss"],
 })
 export class BookComponent implements OnInit {
-
   public books: Book[] = [];
+  public tempBooks: Book[] = [];
+  private search = "";
 
-  constructor(
-    private bookStorageService: BookStorageService
-  ) { }
+  constructor(private bookStorageService: BookStorageService) {}
 
   ngOnInit() {
-    this.bookStorageService.fetchBooks().subscribe(response => {
+    this.bookStorageService.fetchBooks().subscribe((response) => {
       this.books = response;
+      this.tempBooks = [...this.books];
     });
   }
 
+  public onSearch() {
+    var booksSearch: Book[] = [];
+    this.books = [...this.tempBooks];
+    if (this.search.trim() == "") {
+      this.books = this.tempBooks;
+    } else {
+      this.books.forEach((book: Book) => {
+        if (
+          book.title.toLowerCase().includes(this.search.toLowerCase()) ||
+          book.author.toLowerCase().includes(this.search.toLowerCase())
+        ) {
+          booksSearch.push(Object.assign({}, book));
+        }
+      });
+      this.books = [...booksSearch];
+    }
+  }
 }
