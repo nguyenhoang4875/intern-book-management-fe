6650806539c514261b2c5b46e8881a1cb0c36fb5
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-07-07 16:15:55 +0700

    show avatar and update user detail

--- src/app/core/components/layouts/header/header.component.ts
+++ src/app/core/components/layouts/header/header.component.ts
@@ -1,3 +1,5 @@
+import { UserStorageService } from "./../../../../shared/services/user-storage.service";
+import { UserDetail } from "./../../../../shared/model/user-detail.model";
 import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService } from "src/app/shared/services/authentication.service";
@@ -12,9 +14,13 @@ export class HeaderComponent implements OnInit {
   public isLogin: boolean;
   public isAdmin: boolean;
   public currentUser: User;
+  public userDetail: UserDetail = new UserDetail();
   public router: Router;
   public isCollapsed = false;
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userStorageService: UserStorageService
+  ) {}
 
   ngOnInit() {
     this.authenticationService.currentUser.subscribe((user: User) => {
@@ -23,9 +29,13 @@ export class HeaderComponent implements OnInit {
     const user = this.authenticationService.getUserFromLocalStorage();
     if (!user) {
       this.authenticationService.logout();
+    } else {
+      this.userStorageService
+        .getUserById(user.id)
+        .subscribe((response: UserDetail) => {
+          this.userDetail = response;
+        });
     }
     this.isAdmin = this.authenticationService.isAdminRole();
   }
-
-  
 }
